0170c7ef7206cc590c4886d32b25a295842a98a2
BaranyBenedek <benedekbarany@gmail.com>
2023-11-27 17:06:44 +0100

    Starting work on the header of the homepage

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './index.css'
 
-var advert_create = document.getElementById("advert_create");
+var advert_create = document.getElementById("advert_create") as HTMLButtonElement;
 
 advert_create.onclick = function() {
   window.location.href = "./advert.html";
