777a913d5637ac203528715662ce0b9ac0aba4e1
BaranyBenedek <benedekbarany@gmail.com>
2023-11-27 20:52:43 +0100

    Header 95% done

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import './index.css'
 
 var advert_create = document.getElementById("advert_create") as HTMLButtonElement;
+var logo_button = document.getElementById("logo_button") as HTMLLinkElement;
+var kezdolap_button = document.getElementById("kezdolap_button2") as HTMLLinkElement;
 
 advert_create.onclick = function() {
   window.location.href = "./advert.html";
+}
+logo_button.onclick = function() {
+  window.location.href = "./index.html";
+}
+kezdolap_button.onclick = function() {
+  window.location.href = "./index.html";
 }
