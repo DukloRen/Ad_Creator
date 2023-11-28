6f4000a66f4ed1e7ce26b21d467b5e6b46d77fdb
Tóth Kevin <tothkevin0716@gmail.com>
2023-11-28 17:43:57 +0100

    advert_creator.ts navigation code

--- src/advert_creator.ts
+++ src/advert_creator.ts
@@ -1,2 +1,16 @@
 import './index.css'
 import './advert_creator.css'
+
+const advert_create_link = document.getElementById("advert_create_link") as HTMLLinkElement;
+const logo_button = document.getElementById("logo_button") as HTMLLinkElement;
+const kezdolap_button = document.getElementById("kezdolap_button") as HTMLLinkElement;
+
+advert_create_link.onclick = function() {
+  window.location.href = "./advert_creator.html";
+}
+logo_button.onclick = function() {
+  window.location.href = "./index.html";
+}
+kezdolap_button.onclick = function() {
+  window.location.href = "./index.html";
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import './index.css'
 
-var advert_create_link = document.getElementById("advert_create_link") as HTMLLinkElement;
-var advert_create_button = document.getElementById("advert_create_button") as HTMLButtonElement;
-var logo_button = document.getElementById("logo_button") as HTMLLinkElement;
-var kezdolap_button = document.getElementById("kezdolap_button") as HTMLLinkElement;
+const advert_create_link = document.getElementById("advert_create_link") as HTMLLinkElement;
+const advert_create_button = document.getElementById("advert_create_button") as HTMLButtonElement;
+const logo_button = document.getElementById("logo_button") as HTMLLinkElement;
+const kezdolap_button = document.getElementById("kezdolap_button") as HTMLLinkElement;
 
 advert_create_link.onclick = function() {
   window.location.href = "./advert_creator.html";
