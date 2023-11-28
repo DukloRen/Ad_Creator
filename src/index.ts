f809a7df1d0600776a31aece3cfa2537194826b7
Tóth Kevin <tothkevin0716@gmail.com>
2023-11-28 16:50:36 +0100

    Small corrections on homepage

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import './index.css'
 
-var advert_create = document.getElementById("advert_create") as HTMLButtonElement;
+var advert_create_link = document.getElementById("advert_create_link") as HTMLLinkElement;
+var advert_create_button = document.getElementById("advert_create_button") as HTMLButtonElement;
 var logo_button = document.getElementById("logo_button") as HTMLLinkElement;
-var kezdolap_button = document.getElementById("kezdolap_button2") as HTMLLinkElement;
+var kezdolap_button = document.getElementById("kezdolap_button") as HTMLLinkElement;
 
-advert_create.onclick = function() {
+advert_create_link.onclick = function() {
+  window.location.href = "./advert.html";
+}
+advert_create_button.onclick = function() {
   window.location.href = "./advert.html";
 }
 logo_button.onclick = function() {
