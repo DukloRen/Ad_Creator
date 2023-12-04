8b82e190ba53713dce2d7d25378ab53c69813c5e
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-04 17:51:17 +0100

    Moved advert list testing from main.ts to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import { AdvertService } from "./main.ts";
+
 const advert_create_link = document.getElementById("advert_create_link") as HTMLLinkElement;
 const advert_create_button = document.getElementById("advert_create_button") as HTMLButtonElement;
 const logo_button = document.getElementById("logo_button") as HTMLLinkElement;
 const kezdolap_link = document.getElementById("kezdolap_link") as HTMLLinkElement;
 const adatbazis_link = document.getElementById("adatbazis_link") as HTMLLinkElement;
+const advertService = new AdvertService();
 
 advert_create_link.onclick = function() {
   window.location.href = "./advert_creator.html";
@@ -19,3 +22,31 @@ kezdolap_link.onclick = function() {
 adatbazis_link.onclick = function() {
   window.location.href = "https://retoolapi.dev/zmRnPM/advert_test_api";
 }
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const advertTable = document.getElementById("advert") as HTMLTableElement;
+  advertTable.textContent = "";
+  const people = await advertService.getAll();
+  const tableRows = people.map((advert) => {
+    const tr = document.createElement("tr");
+    const titleCol = document.createElement("td");
+    //const imageCol = document.createElement("td");
+    const descriptionCol = document.createElement("td");
+    const categoryCol = document.createElement("td");
+    const priceCol = document.createElement("td");
+    const sellerCol = document.createElement("td");
+    const phonenumberCol = document.createElement("td");
+    const emailCol = document.createElement("td");
+    titleCol.textContent = advert.title;
+    descriptionCol.textContent = advert.description;
+    categoryCol.textContent = advert.category;
+    priceCol.textContent = advert.price.toString();
+    sellerCol.textContent = advert.seller;
+    phonenumberCol.textContent = advert.phonenumber;
+    emailCol.textContent = advert.email;
+    tr.append(...[titleCol, descriptionCol, categoryCol,
+         priceCol, sellerCol, phonenumberCol, emailCol]);
+    return tr;
+  });
+  advertTable.append(...tableRows);
+});
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import './index.css'
 import AdvertInterface from '../interfaces/advert_interface.ts'
 import {AdvertInterfaceDTO} from '../interfaces/advert_interfaceDTO.ts'
 
+//RetoolApi link: https://retoolapi.dev/zmRnPM/advert_test_api
+
 /**
  * 
  */
@@ -23,55 +25,15 @@ export class AdvertService {
           throw new Error("Hiba lépett fel új hirdetés készítése során!");
         }
         return response.json();
-      }
+    }
     
-      async getAll(): Promise<AdvertInterface[]> {
-        const response = await fetch(this.api_url, {
-          headers: { Accept: "application/json" },
-        });
-        if (!response.ok) {
-          throw new Error("Hiba lépett fel a hirdetések kilistázása során!");
-        }
-        return response.json();
+    async getAll(): Promise<AdvertInterface[]> {
+      const response = await fetch(this.api_url, {
+        headers: { Accept: "application/json" },
+      });
+      if (!response.ok) {
+        throw new Error("Hiba lépett fel a hirdetések kilistázása során!");
       }
+      return response.json();
+    }
 }
-
-const advertService = new AdvertService();
-
-document.addEventListener("DOMContentLoaded", () => {
-  listAdverts();
-  document.getElementById("formAdvertCreate")?.addEventListener("submit", (event) => {
-    event.preventDefault();
-    //addAdvert();
-  });
-});
-
-export async function listAdverts() {
-  const advertTable = document.getElementById("advert") as HTMLTableElement;
-  advertTable.textContent = "";
-  const people = await advertService.getAll();
-  const tableRows = people.map((advert) => {
-    const tr = document.createElement("tr");
-    const titleCol = document.createElement("td");
-    //const imageCol = document.createElement("td");
-    const descriptionCol = document.createElement("td");
-    const categoryCol = document.createElement("td");
-    const priceCol = document.createElement("td");
-    const sellerCol = document.createElement("td");
-    const phonenumberCol = document.createElement("td");
-    const emailCol = document.createElement("td");
-    titleCol.textContent = advert.title;
-    descriptionCol.textContent = advert.description;
-    categoryCol.textContent = advert.category;
-    priceCol.textContent = advert.price.toString();
-    sellerCol.textContent = advert.seller;
-    phonenumberCol.textContent = advert.phonenumber;
-    emailCol.textContent = advert.email;
-    tr.append(...[titleCol, descriptionCol, categoryCol,
-         priceCol, sellerCol, phonenumberCol, emailCol]);
-    return tr;
-  });
-  advertTable.append(...tableRows);
-}
-
-//https://retoolapi.dev/zmRnPM/advert_test_api
